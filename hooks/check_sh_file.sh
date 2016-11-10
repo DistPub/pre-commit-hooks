50db8c0531006b4682e7a26d182e21000cb84bbc
Smite Chow <xiaopengyou2no1@gmail.com>
2016-11-10 18:40:39 +0800

    fix hooks

--- hooks/check_sh_file.sh
+++ hooks/check_sh_file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-number=$(find . -name '*.sh'|xargs ls -al|grep -v "\-rwxr\-xr\-x"|wc -l)
+number=$(echo $@|xargs ls -al|grep -v "\-rwxr\-xr\-x"|wc -l)
 number=$(($number + 0))
 if [[ $number != 0 ]]; then
   echo "auto fix *.sh permission."
-  find . -name '*.sh'|xargs chmod uga+x
+  echo $@|xargs chmod uga+x
   exit 1
 fi
