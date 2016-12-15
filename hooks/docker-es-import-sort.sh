2a28622ef2c59e785fefd7c12c1fcf7e71b74b8a
Smite Chow <xiaopengyou2no1@gmail.com>
2016-12-15 22:47:32 +0800

    skip file if get specified comment

--- hooks/docker-es-import-sort.sh
+++ hooks/docker-es-import-sort.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 PATH="/usr/local/bin:$PATH"
 for i in $@ ; do
-  docker exec ugoodspeed_web_1 ./node_modules/import-sort-cli/lib/index.js -o $i
+    skip_file=0
+    while read line; do
+        if [[ $line = '/* eslint sort-imports:0 */' ]]; then
+            skip_file=1
+            break
+        fi
+    done < $i;
+    if [[ $skip_file = 1 ]]; then
+        continue
+    fi
+    docker exec ugoodspeed_web_1 ./node_modules/import-sort-cli/lib/index.js -o $i
 done;
